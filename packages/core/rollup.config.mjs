4111f01a70b1af54a62a02ed1c292a8bc5abb438
Leal Bioley <lb@syware.ch>
2025-04-15 22:05:11 +0200

    Change build to rollup and add umd for cdn

---
+++ packages/core/rollup.config.mjs
@@ -0,0 +1,55 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import dtsPlugin from 'rollup-plugin-dts';
+
+const dts = dtsPlugin.default || dtsPlugin;
+const name = 'Jadis';
+
+export default [
+  // ESM
+  {
+    input: 'build/index.js',
+    output: {
+      file: 'dist/esm/index.mjs', // ✅ One single file
+      format: 'esm',
+      sourcemap: true,
+    },
+    plugins: [resolve(), commonjs()],
+    external: [],
+  },
+
+  // CJS
+  {
+    input: 'build/index.js',
+    output: {
+      file: 'dist/cjs/index.js', // ✅ One single file
+      format: 'cjs',
+      sourcemap: true,
+    },
+    plugins: [resolve(), commonjs()],
+    external: [],
+  },
+
+  // UMD stays the same
+  {
+    input: 'build/index.js',
+    output: {
+      file: 'dist/umd/index.js',
+      format: 'umd',
+      name,
+      sourcemap: true,
+    },
+    plugins: [resolve(), commonjs()],
+    external: [],
+  },
+
+  // Types
+  {
+    input: 'build/index.d.ts',
+    output: {
+      file: 'dist/types/index.d.ts',
+      format: 'es',
+    },
+    plugins: [dts()],
+  },
+];
--- packages/helpers/element.helper.ts
+++ packages/helpers/element.helper.ts
@@ -1,14 +1,14 @@
-import { toKebabCase } from './string.helper';
+import { toKebabCase } from '../string.helperlper';
 
-export const createElement = <T = HTMLElement>(
+export function createElement<T = HTMLElement>(
   tag: string,
   attributes: Record<string, string> = {},
-  appendTo: HTMLElement | ShadowRoot | null = null
-): T => {
+  appendTo?: HTMLElement | ShadowRoot
+): T {
   const el = document.createElement(tag);
   Object.entries(attributes).forEach(([key, value]) =>
     el.setAttribute(toKebabCase(key), value)
   );
   appendTo?.appendChild(el);
   return el as T;
-};
+}
